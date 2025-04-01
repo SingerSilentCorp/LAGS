5906c387b40c27a9937050e9a4376529428e1775
DiegoObregon <diegogh_1234@hotmail.com>
2025-04-01 00:27:07 -0500

    .

--- LAGS/Assets/Scripts/Enemies/TestEnemieAnimations.cs
+++ LAGS/Assets/Scripts/Enemies/TestEnemieAnimations.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class TestEnemieAnimations : MonoBehaviour
 {
     [SerializeField] Transform _player;
-    private enum EnemysStates { walk, escape, dead };
+    private enum EnemysStates { walk,ViewPlayer,Attack, escape, dead };
 
     [SerializeField] EnemysStates _enemie = EnemysStates.walk;
 
@@ -15,7 +15,10 @@ public class TestEnemieAnimations : MonoBehaviour
     private Rigidbody rb;
     Animator _anim;
 
+    float currentX;
     private float lastX;
+
+    [SerializeField]float _speed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,22 +33,11 @@ public class TestEnemieAnimations : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch(_enemie)
+        switch (_enemie)
         {
             case EnemysStates.walk:
 
-                float currentX = transform.position.x; // Posición actual en X
-
-                if (currentX > lastX)
-                {
-                    Debug.Log("Moviéndose a la derecha ➡️");
-                }
-                else if (currentX < lastX)
-                {
-                    Debug.Log("Moviéndose a la izquierda ⬅️");
-                }
-
-                lastX = currentX;
+                MovementAnimationEjeX();
 
 
                 if (health < 5)
@@ -53,40 +45,76 @@ public class TestEnemieAnimations : MonoBehaviour
                     StartCoroutine(StartViewPlayer());
                     _enemie = EnemysStates.escape;
                 }
-                    
-                    break;
-            case EnemysStates.escape:
 
                 
+
+                break;
+
+            case EnemysStates.ViewPlayer:
+
+                FollowPlayer();
+                break;
+
+            case EnemysStates.escape:
+
+
                 if (health < 1)
                 {
                     DEAD();
                     _enemie = EnemysStates.dead;
                 }
-                    
+
                 break;
             case EnemysStates.dead:
-                
+
                 break;
         }
 
         if (Input.GetKeyDown(KeyCode.S)) health -= 2;
     }
 
-    private void ESCAPE()
+    private void MovementAnimationEjeX()
     {
-        _anim.CrossFade("StartBack", 0.0001f);
+         currentX = transform.position.x;
+
+        if (currentX > lastX)
+        {
+            _anim.CrossFade("Right", 0.0001f);
+        }
+        else if (currentX < lastX)
+        {
+            _anim.CrossFade("Left", 0.0001f);
+        }
+
+        lastX = currentX;
     }
 
-    private void DEAD()
-    {
-        _anim.CrossFade("Dead", 0.0001f);
-    }
+    private void ESCAPE() => _anim.CrossFade("StartBack", 0.0001f);
+    
+
+    private void DEAD() => _anim.CrossFade("Dead", 0.0001f);
+    
 
     private IEnumerator StartViewPlayer()
     {
         _anim.CrossFade("StartBack", 0.0001f);
         yield return new WaitForSeconds(_anim.GetCurrentAnimatorStateInfo(0).length);
         _anim.CrossFade("Back", 0.0001f);
+    }
+
+    private bool InRangePlayer2()
+    {
+        return true;
+    
+    }
+
+    private void OutOfRangePlayer()
+    {
+       
+    }
+
+    private void FollowPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _player.position, _speed * Time.deltaTime);
     }
 }
