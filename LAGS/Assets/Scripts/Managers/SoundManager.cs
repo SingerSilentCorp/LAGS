70e69ce16bbc1ac0f1aff0b66d2e496e6f2fed2f
DiegoObregon <diegogh_1234@hotmail.com>
2025-04-05 17:46:58 -0500

    add audio mixer

--- LAGS/Assets/Scripts/Managers/SoundManager.cs
+++ LAGS/Assets/Scripts/Managers/SoundManager.cs
@@ -2,28 +2,35 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    [Header ("Audio Source")]
+    [SerializeField] AudioSource MusicBackground;
+    [SerializeField] AudioSource SFX;
 
-    [SerializeField] AudioClip[] _aud;
-    AudioSource _audioSource;
-        
-    
-    void Start()
-    {
-        _audioSource = GetComponent<AudioSource>();
-    }
+    [Header("Audio Clip")]
+    [SerializeField] AudioClip shootPistola;
+    [SerializeField] AudioClip reloadPistola;
+    [SerializeField] AudioClip shootMetralleta;
+    [SerializeField] AudioClip reloadMetralleta;
+    [SerializeField] AudioClip shootEscopeta;
+    [SerializeField] AudioClip reloadEscopeta;
+    [SerializeField] AudioClip caminataEnemie1;
+    [SerializeField] AudioClip caminataEnemie2;
+    [SerializeField] AudioClip caminataEnemie3;
+    [SerializeField] AudioClip caminataJefe;
 
 
 
 
-    public void ShootPistola() => _audioSource.PlayOneShot(_aud[0]);
-    public void ReloadPistola() => _audioSource.PlayOneShot(_aud[0]);
-    public void ShootMetralleta() => _audioSource.PlayOneShot(_aud[0]);
-    public void ReloadMetralleta() => _audioSource.PlayOneShot(_aud[0]);
-    public void ShootEscopeta() => _audioSource.PlayOneShot(_aud[0]);
-    public void ReloadEscopeta() => _audioSource.PlayOneShot(_aud[0]);
-    public void CaminataEnemie1() => _audioSource.PlayOneShot(_aud[0]);
-    public void CaminataEnemie2() => _audioSource.PlayOneShot(_aud[0]);
-    public void CaminataEnemie3() => _audioSource.PlayOneShot(_aud[0]);
-    public void CaminataJefe() => _audioSource.PlayOneShot(_aud[0]);
+
+    public void ShootPistola() => SFX.PlayOneShot(shootPistola);
+    public void ReloadPistola() => SFX.PlayOneShot(reloadPistola);
+    public void ShootMetralleta() => SFX.PlayOneShot(shootMetralleta);
+    public void ReloadMetralleta() => SFX.PlayOneShot(reloadMetralleta);
+    public void ShootEscopeta() => SFX.PlayOneShot(shootEscopeta);
+    public void ReloadEscopeta() => SFX.PlayOneShot(reloadEscopeta);
+    public void CaminataEnemie1() => SFX.PlayOneShot(caminataEnemie1);
+    public void CaminataEnemie2() => SFX.PlayOneShot(caminataEnemie2);
+    public void CaminataEnemie3() => SFX.PlayOneShot(caminataEnemie3);
+    public void CaminataJefe() => SFX.PlayOneShot(caminataJefe);
 
 }
